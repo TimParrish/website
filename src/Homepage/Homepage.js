f49d1fd0ddc1e9beddb16bbb8e9ee5bd29cb4e14
Tim Parrish <timothyjparrish@gmail.com>
2019-08-19 11:50:02 -0600

    add Coverflow to home page

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -1,15 +1,76 @@
 import React from "react";
 import SedanMT from "images/Sedan-MT.jpg";
+import BigHorn from "images/bigHornSheep.jpg";
+import BuckElkRMNP from "images/buckElkRMNP.jpg";
+import BuckElkYNP from "images/buckElkYNP.jpg";
+import Chipmunk from "images/chipmunk.jpg";
+import Chipmunk2 from "images/chipmunk2.jpg";
+import ChipmunkRMNP from "images/chipmunkRMNP.jpg";
 import { Heading, HeroImg, CenteredP } from "styles";
+import Coverflow from "react-coverflow";
 
 function Homepage() {
   return (
     <>
-      <HeroImg src={SedanMT} alt="Sedan, Montana" />
+      <Coverflow
+        displayQuantityOfSide={1}
+        enableScroll
+        clickable={true}
+        active={0}
+        enableHeading={false}
+        currentFigureScale={2}
+        media={{
+          "@media (max-width: 900px)": {
+            width: "600px",
+            height: "300px"
+          },
+          "@media (min-width: 900px)": {
+            width: "960px",
+            height: "600px"
+          }
+        }}
+      >
+        <img
+          src={SedanMT}
+          alt="Sedan, Montana"
+          data-action="http://andyyou.github.io/react-coverflow/"
+        />
+        <img
+          src={BigHorn}
+          alt="Yellowstone National Park, Wyoming"
+          data-action="http://andyyou.github.io/react-coverflow/"
+        />
+        <img
+          src={BuckElkRMNP}
+          alt="Buck Elk in Rocky Mountain National Park, CO"
+          data-action="http://andyyou.github.io/react-coverflow/"
+        />
+        <img
+          src={Chipmunk}
+          alt="Chipmonk 1"
+          data-action="http://andyyou.github.io/react-coverflow/"
+        />
+        <img
+          src={Chipmunk2}
+          alt="Chipmonk 2"
+          data-action="http://andyyou.github.io/react-coverflow/"
+        />
+        <img
+          src={ChipmunkRMNP}
+          alt="Chipmonk Rocky Mountain National Park, CO"
+          data-action="http://andyyou.github.io/react-coverflow/"
+        />
+        <img
+          src={BuckElkYNP}
+          alt="Buck Elk in Yellowston National Park, Wyoming"
+          data-action="http://andyyou.github.io/react-coverflow/"
+        />
+      </Coverflow>
       <Heading>Welcome to my first deployed React App!</Heading>
       <CenteredP>
         This is still a work in progress but will eventually replace the static
-        site that can be found at www.timothyparrish.com
+        site that can be found at{" "}
+        <a href="http://www.timothyparrish.com">www.timothyparrish.com</a>
       </CenteredP>
     </>
   );
