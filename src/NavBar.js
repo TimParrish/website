c805314edfb814a1d985df8b1bca269b7bb3f2c3
Tim Parrish <timothyjparrish@gmail.com>
2019-08-22 13:34:10 -0600

    updated nav bar

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,8 +4,8 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
   return (
     <>
-      <NavLink to="/">HomePage</NavLink>
-      <NavLink to="/blackjack">Blackjack</NavLink>
+      <NavLink to="/website/">HomePage</NavLink>
+      <NavLink to="/website/blackjack">Blackjack</NavLink>
     </>
   );
 }
